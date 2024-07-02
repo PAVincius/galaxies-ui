69d2ddb6a19c13cf3d0e43ff7d57aeead5b6bc05
PAVincius <vinicius.pratesaraujo@gmail.com>
2024-07-02 00:17:39 -0300

    docs(site-config): replacing github link

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
     "From dust to starts. Set up your project at lightning speed.",
   links: {
     twitter: "https://twitter.com/BylkaYf",
-    github: "https://github.com/BelkacemYerfa/shadcn-extension",
+    github: "https://github.com/PAVincius/galaxies-ui",
     docs: "/docs/introduction",
     components: "/components",
   },
